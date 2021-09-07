294aed73459fc76db2489f78e07d4eea1150d69d
iia7mt <a.ghannam18@student.aaup.edu>
2021-09-07 18:39:51 +0300

    threads status

--- app/src/main/java/com/medo/rxjava/MainActivity.java
+++ app/src/main/java/com/medo/rxjava/MainActivity.java
@@ -13,10 +13,12 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.core.SingleObserver;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.internal.observers.BiConsumerSingleObserver;
 import io.reactivex.rxjava3.observables.ConnectableObservable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.subjects.AsyncSubject;
 import io.reactivex.rxjava3.subjects.BehaviorSubject;
 import io.reactivex.rxjava3.subjects.PublishSubject;
@@ -224,6 +226,9 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        };
 //            observable.subscribe(observer);
+            Observable.just(1,2,3,4,5)
+                    .doOnNext(c->Log.d(TAG,"upStream"+c+"\tthread :"+Thread.currentThread().getName())).subscribeOn(Schedulers.computation())
+                    .subscribe(o->Log.d(TAG,"dnStream"+o+"\tthread :"+Thread.currentThread().getName()));
 
     }
 
